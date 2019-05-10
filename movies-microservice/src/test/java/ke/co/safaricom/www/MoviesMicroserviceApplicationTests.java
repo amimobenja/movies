13c651ecae8be2d835276a6795bb7214551ff983
Amimo Benja <amimobenja08@gmail.com>
2019-05-10 07:03:37 +0300

    Updated test cases

--- movies-microservice/src/test/java/ke/co/safaricom/www/MoviesMicroserviceApplicationTests.java
+++ movies-microservice/src/test/java/ke/co/safaricom/www/MoviesMicroserviceApplicationTests.java
@@ -10,9 +10,6 @@ import org.junit.Test;
 import static ke.co.safaricom.www.testconstants.TestCasesConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import org.junit.Before;
 import org.junit.runner.RunWith;
@@ -21,7 +18,6 @@ import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import static org.springframework.http.converter.json.Jackson2ObjectMapperBuilder.json;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
